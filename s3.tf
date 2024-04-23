330c0e3eb33886d6c9e5ed6ff2ad76cb257b5b3f
Jonathan Magee <120496483+jmagee70@users.noreply.github.com>
2024-04-23 14:20:17 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,28 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
-  }
-}
-
-module "s3_bucket_for_logs" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket-for-logs"
-  acl    = "log-delivery-write"
-
-  # Allow deletion of non-empty bucket
-  force_destroy = true
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  attach_elb_log_delivery_policy = true
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.2"
 }
