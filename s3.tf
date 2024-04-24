1229860025a1766f0322854bb779896ca975225c
Jonathan Magee <120496483+jmagee70@users.noreply.github.com>
2024-04-24 09:34:07 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,4 +1,10 @@
-module "s3-bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.1.2"
+provider "aws" {
+  region     = "eu-XXXXX-1"
+  access_key = "XXXXXXXXXXX
+  secret_key = "XXXXXXXXXXXX"
+}
+
+resource "aws_s3_bucket" "jmdrift-bucket" {
+  bucket = "jm-this-bucket-should-drift"
+  
 }
