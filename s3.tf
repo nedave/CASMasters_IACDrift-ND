e64a63818ab2c7538e9b2a938f19861a49aa78af
Neel <nedave@paloaltonetworks.com>
2024-06-12 11:00:38 +0100

    added Yor tag

--- s3.tf
+++ s3.tf
@@ -1,10 +1,14 @@
 provider "aws" {
-  region                  = "eu-west-2"
+  region                   = "eu-west-2"
   shared_credentials_files = ["/Users/nedave/.aws/credentials"]
-  profile                 = "434843555728_AWSAdministratorAccess"
+  profile                  = "434843555728_AWSAdministratorAccess"
 }
 
 resource "aws_s3_bucket" "nddrift-bucket" {
   bucket = "nd-this-bucket-should-drift"
-  
+
+  tags = {
+    yor_name  = "nddrift-bucket"
+    yor_trace = "c04ae722-7b13-4c8f-8220-e0a44f44b000"
+  }
 }
